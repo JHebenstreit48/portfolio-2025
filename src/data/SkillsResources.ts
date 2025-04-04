9bb72643074dc7f2d1237bdd350e16017094b87c
Justin <j.hebenstreit25@gmail.com>
2025-04-03 19:52:11 -0500

    added more resource links and updated Skills icons

--- src/data/SkillsResources.ts
+++ src/data/SkillsResources.ts
@@ -1,93 +1,216 @@
-export const SkillsResources: Record<string, { label: string; url: string }[]> =
-  {
-    HTML: [
-      {
-        label: "MDN HTML Docs",
-        url: "https://developer.mozilla.org/en-US/docs/Web/HTML"
-      },
-      {
-        label: "W3Schools HTML Tutorial",
-        url: "https://www.w3schools.com/html/",
-      }
-    ],
-    CSS: [
-      {
-        label: "MDN CSS Docs",
-        url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
-      },
-      {
-        label: "W3Schools CSS Tutorial",
-        url: "https://www.w3schools.com/css/",
-      },
-    ],
-    JavaScript: [
-      {
-        label: "MDN JavaScript Docs",
-        url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-      },
-    ],
-    TypeScript: [
-      { label: "TypeScript Docs", url: "https://www.typescriptlang.org/docs" },
-    ],
-    React: [{ label: "React Docs", url: "https://react.dev/" }],
-    Node: [{ label: "Node.js Docs", url: "https://nodejs.org/en/docs" }],
-    MongoDB: [{ label: "MongoDB Docs", url: "https://www.mongodb.com/docs/" }],
-    PostgreSQL: [
-      { label: "PostgreSQL Docs", url: "https://www.postgresql.org/docs/" },
-    ],
-    Angular: [{ label: "Angular Docs", url: "https://angular.io/docs" }],
-    Apollo: [
-      {
-        label: "Apollo GraphQL Docs",
-        url: "https://www.apollographql.com/docs/",
-      },
-    ],
-    GraphQL: [{ label: "GraphQL Docs", url: "https://graphql.org/learn" }],
-    Vue: [
-      {
-        label: "Vue.js Docs",
-        url: "https://vuejs.org/guide/introduction.html",
-      },
-    ],
-    Drupal: [{ label: "Drupal Docs", url: "https://www.drupal.org/docs" }],
-    Git: [{ label: "Git Docs", url: "https://git-scm.com/doc" }],
-    GitHub: [{ label: "GitHub Docs", url: "https://docs.github.com/en" }],
-    Python: [{ label: "Python Docs", url: "https://docs.python.org/3/" }],
-    CSharp: [
-      {
-        label: "C# Docs",
-        url: "https://learn.microsoft.com/en-us/dotnet/csharp/",
-      },
-    ],
-    Unity: [
-      {
-        label: "Unity Manual",
-        url: "https://docs.unity3d.com/Manual/index.html",
-      },
-    ],
-    Dart: [{ label: "Dart Docs", url: "https://dart.dev/guides" }],
-    Flutter: [{ label: "Flutter Docs", url: "https://docs.flutter.dev/" }],
-    Swift: [
-      {
-        label: "Swift Docs",
-        url: "https://developer.apple.com/documentation/swift",
-      },
-    ],
-    Expo: [{ label: "Expo Docs", url: "https://docs.expo.dev/" }],
-    ReactNative: [
-      {
-        label: "React Native Docs",
-        url: "https://reactnative.dev/docs/getting-started",
-      },
-    ],
-    VSCode: [
-      { label: "VS Code Docs", url: "https://code.visualstudio.com/docs" },
-    ],
-    Postman: [
-      {
-        label: "Postman Learning Center",
-        url: "https://learning.postman.com/",
-      },
-    ],
-    Insomnia: [{ label: "Insomnia Docs", url: "https://docs.insomnia.rest/" }],
-  };
+export const SkillsResources: Record<string, { label: string; url: string }[]> = {
+  // ===============================
+  // Web Development – Front End
+  // ===============================
+  Angular: [
+    {
+      label: "Angular Docs",
+      url: "https://angular.io/docs"
+    }
+  ],
+  CSS: [
+    {
+      label: "MDN CSS Docs",
+      url: "https://developer.mozilla.org/en-US/docs/Web/CSS"
+    },
+    {
+      label: "W3Schools CSS Tutorial",
+      url: "https://www.w3schools.com/css/"
+    }
+  ],
+  HTML: [
+    {
+      label: "MDN HTML Docs",
+      url: "https://developer.mozilla.org/en-US/docs/Web/HTML"
+    },
+    {
+      label: "W3Schools HTML Tutorial",
+      url: "https://www.w3schools.com/html/"
+    }
+  ],
+  JavaScript: [
+    {
+      label: "MDN JavaScript Docs",
+      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+    }
+  ],
+  TypeScript: [
+    {
+      label: "TypeScript Docs",
+      url: "https://www.typescriptlang.org/docs"
+    }
+  ],
+  React: [
+    {
+      label: "React Docs",
+      url: "https://react.dev/"
+    }
+  ],
+  Sass: [
+    {
+      label: "Sass Docs",
+      url: "https://sass-lang.com/documentation/"
+    }
+  ],
+  Vue: [
+    {
+      label: "Vue.js Docs",
+      url: "https://vuejs.org/guide/introduction.html"
+    }
+  ],
+  Drupal: [
+    {
+      label: "Drupal Docs",
+      url: "https://www.drupal.org/docs"
+    }
+  ],
+
+  // ===============================
+  // Web Development – Back End
+  // ===============================
+  Apollo: [
+    {
+      label: "Apollo GraphQL Docs",
+      url: "https://www.apollographql.com/docs/"
+    }
+  ],
+  GraphQL: [
+    {
+      label: "GraphQL Docs",
+      url: "https://graphql.org/learn"
+    }
+  ],
+  Kotlin: [
+    {
+      label: "Kotlin Docs",
+      url: "https://kotlinlang.org/docs/android-overview.html"
+    }
+  ],
+  MongoDB: [
+    {
+      label: "MongoDB Docs",
+      url: "https://www.mongodb.com/docs/"
+    }
+  ],
+  Node: [
+    {
+      label: "Node.js Docs",
+      url: "https://nodejs.org/en/docs"
+    }
+  ],
+  PostgreSQL: [
+    {
+      label: "PostgreSQL Docs",
+      url: "https://www.postgresql.org/docs/"
+    }
+  ],
+  Python: [
+    {
+      label: "Python Docs",
+      url: "https://docs.python.org/3/"
+    }
+  ],
+
+  // ===============================
+  // Mobile Development
+  // ===============================
+  Dart: [
+    {
+      label: "Dart Docs",
+      url: "https://dart.dev/guides"
+    }
+  ],
+  Expo: [
+    {
+      label: "Expo Docs",
+      url: "https://docs.expo.dev/"
+    }
+  ],
+  Flutter: [
+    {
+      label: "Flutter Docs",
+      url: "https://docs.flutter.dev/"
+    }
+  ],
+  ReactNative: [
+    {
+      label: "React Native Docs",
+      url: "https://reactnative.dev/docs/getting-started"
+    }
+  ],
+  Swift: [
+    {
+      label: "Swift Docs",
+      url: "https://developer.apple.com/documentation/swift"
+    }
+  ],
+
+  // ===============================
+  // Game Development
+  // ===============================
+  C: [
+    {
+      label: "C Docs Microsoft",
+      url: "https://learn.microsoft.com/en-us/cpp/c-language/?view=msvc-170"
+    },
+    {
+      label: "C Tutorial W3Schools",
+      url: "https://www.w3schools.com/c/"
+    }
+  ],
+  CSharp: [
+    {
+      label: "C# Docs",
+      url: "https://learn.microsoft.com/en-us/dotnet/csharp/"
+    }
+  ],
+  Unity: [
+    {
+      label: "Unity Manual",
+      url: "https://docs.unity3d.com/Manual/index.html"
+    }
+  ],
+
+  // ===============================
+  // Tools & Utilities
+  // ===============================
+  Git: [
+    {
+      label: "Git Docs",
+      url: "https://git-scm.com/doc"
+    }
+  ],
+  GitHub: [
+    {
+      label: "GitHub Docs",
+      url: "https://docs.github.com/en"
+    }
+  ],
+  Insomnia: [
+    {
+      label: "Insomnia Docs",
+      url: "https://docs.insomnia.rest/"
+    }
+  ],
+  Postman: [
+    {
+      label: "Postman Learning Center",
+      url: "https://learning.postman.com/"
+    }
+  ],
+  VSCode: [
+    {
+      label: "VS Code Docs",
+      url: "https://code.visualstudio.com/docs"
+    }
+  ],
+
+  // ===============================
+  // Cisco / IT Networking (TODO)
+  // ===============================
+  // Placeholder for future networking resources like:
+  // - Cisco Packet Tracer
+  // - CCNA Study Guides
+  // - NetAcad
+  // - CompTIA Network+ Docs
+};

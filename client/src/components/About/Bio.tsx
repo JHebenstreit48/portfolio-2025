b81fc16bccb04de31ee54ad81e5e3a33b11b21f8
Justin <j.hebenstreit25@gmail.com>
2025-04-17 19:18:45 -0500

    changed profile picture and adjusted some SASS/SCSS

--- client/src/components/About/Bio.tsx
+++ client/src/components/About/Bio.tsx
@@ -2,45 +2,41 @@ import '@/SCSS/AboutMe/Bio.scss';
 
 export default function Bio() {
     return (
-        <div>
-            <div className='about'>
+        <div className="about">
 
-                <p className="bio">
-                    I am a Full-Stack Web Developer with a rich background in photography and music, complemented by a deep-seated passion for automotive technology and gaming. In November 2024, I completed a comprehensive 6-month Full-Stack Web Development Boot Camp through Washington University in St. Louis, offered in partnership with edX. This intensive program honed my skills in technologies such as HTML, CSS, JavaScript, TypeScript, Node.js, Express.js, PostgreSQL, and React.
-                </p>
+            <p className='bio'>
+                I am a Full-Stack Web Developer with a rich background in photography and music, complemented by a deep-seated passion for automotive technology and gaming. In November 2024, I completed a comprehensive 6-month Full-Stack Web Development Boot Camp through Washington University in St. Louis, offered in partnership with edX. This intensive program honed my skills in technologies such as HTML, CSS, JavaScript, TypeScript, Node.js, Express.js, PostgreSQL, and React.
+            </p>
 
-                <div>
-                    <p className="learning">
-                        Dedicated to continuous learning, I am actively expanding my expertise across both front-end and back-end technologies. My current areas of focus include:
-                    </p>
+            <p className='bio'>
+                Dedicated to continuous learning, I am actively expanding my expertise across both front-end and back-end technologies. My current areas of focus include:
+            </p>
 
-                    <ul className="currentFocus">
-                        <li>
-                            Front-End Development: Delving into frameworks and tools like Angular, Vue.js, Redux, SASS/SCSS, and Tailwind CSS to enhance the user interface and experience.
-                        </li>
-                        <li>
-                            Back-End Development: Exploring server-side technologies such as Python and Django, and integrating GraphQL with Apollo to optimize data management and API interactions.
-                        </li>
-                        <li>
-                            Mobile Development: Learning React Native to build cross-platform mobile applications that deliver seamless user experiences.
-                        </li>
-                        <li>
-                            DevOps and Databases: Gaining proficiency with Docker for containerization, Firebase for scalable backend solutions, and databases like MySQL and Redis to ensure efficient data storage and retrieval.
-                        </li>
-                        <li>
-                            Additionally, I have a burgeoning interest in game development, currently teaching myself C# and exploring related topics to diversify my programming capabilities.
-                        </li>
-                    </ul>
-                </div>
+            <ul className="bio">
+                <li>
+                    Front-End Development: Delving into frameworks and tools like Angular, Vue.js, Redux, SASS/SCSS, and Tailwind CSS to enhance the user interface and experience.
+                </li>
+                <li>
+                    Back-End Development: Exploring server-side technologies such as Python and Django, and integrating GraphQL with Apollo to optimize data management and API interactions.
+                </li>
+                <li>
+                    Mobile Development: Learning React Native to build cross-platform mobile applications that deliver seamless user experiences.
+                </li>
+                <li>
+                    DevOps and Databases: Gaining proficiency with Docker for containerization, Firebase for scalable backend solutions, and databases like MySQL and Redis to ensure efficient data storage and retrieval.
+                </li>
+                <li>
+                    Additionally, I have a burgeoning interest in game development, currently teaching myself C# and exploring related topics to diversify my programming capabilities.
+                </li>
+            </ul>
 
-                <div className='bio'>
-                    In 2019, I earned Cisco's CCNA Security (210-260) and CCNA Collaboration (210-060) certifications. Although these certifications expired in 2022 following Cisco's certification restructuring in 2020, I am actively refreshing my knowledge in these areas with the goal of recertification, ensuring my networking skills remain current and robust.
-                </div>
+            <p className='bio'>
+                In 2019, I earned Cisco's CCNA Security (210-260) and CCNA Collaboration (210-060) certifications. Although these certifications expired in 2022 following Cisco's certification restructuring in 2020, I am actively refreshing my knowledge in these areas with the goal of recertification, ensuring my networking skills remain current and robust.
+            </p>
 
-                <div className='bio'>
-                    As a creative thinker and collaborative team player, I am eager to contribute my diverse skill set and ever-expanding knowledge base to innovative development teams, embracing challenges that foster both personal and professional growth.
-                </div>
-            </div>
+            <p className='bio'>
+                As a creative thinker and collaborative team player, I am eager to contribute my diverse skill set and ever-expanding knowledge base to innovative development teams, embracing challenges that foster both personal and professional growth.
+            </p>
 
         </div>
     )

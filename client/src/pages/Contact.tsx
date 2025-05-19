a811c9e562b3be35a9a8b04bce09794a8be4cd6d
Justin <j.hebenstreit25@gmail.com>
2025-05-19 18:43:59 -0500

    started prepping to connect front and back end

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -1,15 +1,12 @@
-import ContactForm from "@/components/ContactForm"
+import ContactForm from "@/components/ContactForm";
+
 export default function Contact() {
   return (
     <>
-
       <h1 className="pageHeader">Contact</h1>
-
       <div>
         <ContactForm />
       </div>
-
-
     </>
-  )
+  );
 }
